1b5be010de1e009fc4a40e73b4352580787dc9a7
EwertonBar <80701116+EwertonBar@users.noreply.github.com>
2021-06-02 14:00:25 -0300

    Atualização
    Material desenvolvido durante aula

---
+++ aula01.js
@@ -0,0 +1,8 @@
+var vel = 59.5
+console.log(`A velocidade do carro é ${vel}km/h`)
+console.log(`Dirija sempre usando cinto de segurança`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade de 60km/h`)    
+} else {
+    console.log(`Você não ultrapassou a velocidade de 60km/h`);
+}
